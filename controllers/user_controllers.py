f0b8f945e7ba922e59b7d610c7f2248dfb243d06
Nguyen Long <longlcf0311@gmail.com>
2022-07-13 16:37:13 +0700

    add jwt

--- controllers/user_controllers.py
+++ controllers/user_controllers.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, url_for, request, redirect, session, jsonify, send_file, Blueprint
+from datetime import datetime, timedelta
+from flask import Flask, render_template, url_for, request, redirect, session, jsonify, send_file, Blueprint, make_response
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
 from model import User, db
-
+import jwt
+from config import SECRET_KEY
+from configs.utils import *
 auth_router = Blueprint('auth_router', __name__)
 
 
@@ -14,13 +17,23 @@ def register_func():
         password = generate_password_hash(password)
         gender = request.form['gender']
         birthday = request.form['birthday']
-        user = User(username=username, password=password, gender=gender,
-                    birthday=birthday)
-        db.session.add(user)
-        db.session.commit()
+
+        user = User.query\
+            .filter_by(username=username)\
+            .first()
+        if not user:
+
+            user = User(username=username, password=password, gender=gender,
+                        birthday=birthday)
+
+            db.session.add(user)
+            db.session.commit()
+            return jsonify(user.toDict()), 200
+        else:
+            return make_response('User already exists. Please Log in.', 202)
+
     except Exception as e:
         return jsonify({"error": "Exception: {}".format(e)}), 400
-    return jsonify(user.toDict()), 200
 
 
 @auth_router.route('/api/login', methods=["POST"])
@@ -30,12 +43,27 @@ def login_func():
         password = request.form['password']
 
         user = User.query.filter_by(username=username).first()
+        if not user:
+            return make_response(
+                'Could not verify',
+                401
+            )
 
         if check_password_hash(user.password, password):
-            return jsonify(user.toDict()), 200
+            token = jwt.encode({
+                'username': user.username,
+                'exp': datetime.utcnow() + timedelta(minutes=10000)
+            }, SECRET_KEY)
+            return make_response(jsonify({'token': token.decode('UTF-8'), 'user': user.toDict()}), 201)
         else:
             return jsonify({
                 "msg": "Something went wrong"
             }), 400
     except Exception as e:
         return jsonify({'Error': "{}".format(e)}), 500
+
+
+@auth_router.route('/api/me', methods=['GET'])
+@token_required
+def get_user(current_user):
+    return jsonify({'users': current_user.toDict()})
